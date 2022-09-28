5ad0462bbfd7e2d6c711a0f8a5f01f4f2bdb689d
advikg <advik.sd@gmail.com>
2022-09-28 16:22:56 -0700

    completed variables assignment

---
+++ Week 2/Variables/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main (String[] args) {
+        int age = 14;
+        int height = 69;
+        char first_letter = 'A';
+        String last_name = "Garg";
+        String programming_best = "Programming is not the best, hot juicy men are superior than programming";
+        System.out.println("Hi, my name is " + first_letter + " " + last_name);
+        System.out.println("I am " + age + " years old and I am " + height + " inches tall");
+        System.out.println(programming_best);
+    }
+}
