c4168ee63f232b1e4c5b7060e726a73cbd30621c
advikg <advik.sd@gmail.com>
2022-10-04 16:16:09 -0700

    Finished arrays activity

---
+++ Week 2/Arrays/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main (String[] args) {
+        String[] array = {"Software","Your Mother.", "idfk", "Deep Space"};
+        System.out.println(array[3]);
+        System.out.println(array[2]);
+        System.out.println(array[1]);
+        System.out.println(array[0]);
+    }
+}
