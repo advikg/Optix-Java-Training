ef3f4f9c699cdaf9ba7b34126998ce462fe726e0
advikg <advik.sd@gmail.com>
2022-10-04 16:34:36 -0700

    Completed Arithmetics Assignment

---
+++ Week 2/Arithmetics/Arithmetics.java
@@ -0,0 +1,7 @@
+public class Arithmetics {
+    public static void main (String[] args) {
+        double num = (6 * 1234124 / 11354 - 890809 + 90800) % 980098;
+        num /= 100.0;
+        System.out.println(num);
+    }
+}
