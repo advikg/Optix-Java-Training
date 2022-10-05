c01af89d2185b03a7ef51cef217816762edd3416
advikg <advik.sd@gmail.com>
2022-10-04 17:02:00 -0700

    Completed For Loops Assignment

---
+++ Week 2/For Loops/ForLoops.java
@@ -0,0 +1,8 @@
+public class ForLoops {
+    public static void main (String[] args) {
+        int[] big_juicy_men = {1, 2, 3, 4, 5};
+        for (int balls = 0; balls < big_juicy_men.length; balls++ ) {
+            System.out.println(big_juicy_men[balls]);
+        }
+    }
+}
