71a9f6fc61b03778d24fa4aed035c6e310598215
advikg <advik.sd@gmail.com>
2022-10-17 21:39:22 -0700

    Completed non-void assignments

---
+++ Week 3/Non-Void Methods/Non_void_methods.java
@@ -0,0 +1,18 @@
+public class Non_void_methods {
+    public static String albertian_economics (String albert) {
+        albert = albert + " is friends with Albert";
+        return albert;
+    }
+    public static int doubling (int number) {
+        number = number * 2;
+        return number;
+    }
+    public static boolean is_optix_enjoyer (boolean common_optix_L) {
+        return !common_optix_L;
+    }
+    public static void main (String[] args) {
+        System.out.println(albertian_economics("Oddvik"));
+        System.out.println(doubling(3749));
+        System.out.println(is_optix_enjoyer(false));
+    }
+}
